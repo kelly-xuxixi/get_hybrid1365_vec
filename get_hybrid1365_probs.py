574ee2eb0d42878f02977dd147693e70e6f7b14f
kelly-xuxixi <nflsxcx@126.com>
2018-05-14 16:57:45 +0800

    n

--- get_hybrid1365_probs.py
+++ get_hybrid1365_probs.py
@@ -7,36 +7,39 @@ os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 
 
 root_folder = '/m/data/med/frame'
+probs_folder = '/m/data/med/probs'
 
 
 def get_1365_vec(vgg, input_tensor, sess, folder_path):
     print(folder_path)
-    files = os.listdir(folder_path)
-    imgs = []
-    num = 0
-    for file in files:
-        if not os.path.isdir(file) and file.endswith('.jpg'):
-            image = utils.load_image(os.path.join(folder_path, file))
-            imgs.append(image)
+    try:
+        files = os.listdir(folder_path)
+        imgs = []
+        num = 0
+        for file in files:
+            if not os.path.isdir(file) and file.endswith('.jpg'):
+                image = utils.load_image(os.path.join(folder_path, file))
+                imgs.append(image)
+                num += 1
+        while num % 64 != 0:
+            imgs.append(np.zeros([224, 224, 3]))
             num += 1
-    while num % 64 != 0:
-        imgs.append(np.zeros([224, 224, 3]))
-        num += 1
-    imgs = np.stack(imgs)
-    print(imgs.shape)
-    all_probs = []
-    for i in range(int(len(imgs) / 64)):
-        feed_dict = {input_tensor: imgs[i * 64: (i + 1) * 64]}
-        probs = sess.run(vgg.prob, feed_dict=feed_dict)
-        all_probs.append(probs)
-    all_probs = np.vstack(all_probs)
-    all_probs = all_probs[:len(files)]
-    print(all_probs.shape)
-    utils.print_prob(all_probs[0], './synset.txt')
-    utils.print_prob(all_probs[1], './synset.txt')
-    utils.print_prob(all_probs[2], './synset.txt')
-
-    return all_probs
+        imgs = np.stack(imgs)
+        print(imgs.shape)
+        all_probs = []
+        for i in range(int(len(imgs) / 64)):
+            feed_dict = {input_tensor: imgs[i * 64: (i + 1) * 64]}
+            probs = sess.run(vgg.prob, feed_dict=feed_dict)
+            all_probs.append(probs)
+        all_probs = np.vstack(all_probs)
+        all_probs = all_probs[:len(files)]
+        print(all_probs.shape)
+        utils.print_prob(all_probs[0], './synset.txt')
+        utils.print_prob(all_probs[1], './synset.txt')
+        utils.print_prob(all_probs[2], './synset.txt')
+        return all_probs
+    except Exception as e:
+        print(e)
 
 
 def main():
@@ -51,7 +54,7 @@ def main():
         for folder in folders:
             folder_path = os.path.join(root_folder, folder)
             probs_for_folder = get_1365_vec(vgg, input_tensor, sess, folder_path)
-            np.savetxt(os.path.join(folder_path, 'probs.txt'), probs_for_folder)
+            np.savetxt(os.path.join(probs_folder, folder + '.txt'), probs_for_folder)
 
 
 if __name__ == '__main__':
